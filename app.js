3790ffeb77c634e2eae149a09e2ef4e54885b567
Anusha Tungala <tungalaanusha16@gmail.com>
2026-06-29 15:37:21 +0530

    Updated to Version 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Version 1");
+    res.end("Version 2");
 });
 
 server.listen(3000, () => {
